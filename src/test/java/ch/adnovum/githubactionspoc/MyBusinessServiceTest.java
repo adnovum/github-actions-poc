6b1f7348d0451cf05385fcebd26bdd9517810bf6
Sandro Heinzelmann <sandro.heinzelmann@adnovum.ch>
2019-12-13 10:40:35 +0100

    See what happens when tests fail

--- src/test/java/ch/adnovum/githubactionspoc/MyBusinessServiceTest.java
+++ src/test/java/ch/adnovum/githubactionspoc/MyBusinessServiceTest.java
@@ -2,13 +2,19 @@ package ch.adnovum.githubactionspoc;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class MyBusinessServiceTest {
 
 	@Test
-	public void shouldDoStuff() {
+	public void shouldAdd() {
 		int sum = new MyBusinessService().add(10, 3);
 		assertEquals(13, sum);
+	}
+
+	@Test
+	public void shouldFail() {
+		Assert.fail("I'm a failure!");
 	}
 }
